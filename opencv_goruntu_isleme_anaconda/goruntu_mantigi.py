3a6ed3ef9b5ed80aa52a99858791b00678eb4020
Mehmet Alp ARSLAN <5alparslanmehmet8@gmail.com>
2026-02-27 12:16:01 +0300

    goruntu mantigi

--- opencv_goruntu_isleme_anaconda/goruntu_mantigi.py
+++ opencv_goruntu_isleme_anaconda/goruntu_mantigi.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
-img = np.zeros((100,30,2), dtype=np.uint8)
+img = np.zeros((100,30,3), dtype=np.uint8)
 
-cv2.imshow("Canvas", img)
+
+
 
 cv2.resize(img, (1000,1000), interpolation=cv2.INTER_AREA)
+cv2.imshow("Canvas", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
